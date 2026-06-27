fd6c6693e5d403c96d7eb5c6be11fd2239a1d535
Lam II <61718883+lamlg00@users.noreply.github.com>
2026-06-27 05:59:37 -0500

    Delete cona-adventures-nextjs/cona-adventures directory

--- cona-adventures-nextjs/cona-adventures/components/Toast.js
+++
@@ -1,8 +0,0 @@
-// components/Toast.js
-import { useApp } from '../context/AppContext';
-
-export default function Toast() {
-  const { toast } = useApp();
-  if (!toast) return null;
-  return <div className="toast">{toast}</div>;
-}
